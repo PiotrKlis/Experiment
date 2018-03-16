72b2f58e74bb160ad6976dfade1ae5c06f745976
piotrklis <piotr.klis@tigerspike.com>
2018-03-16 21:13:55 +0100

    saveInstance

--- app/src/main/java/com/piotrklis/experiment/MainActivity.java
+++ app/src/main/java/com/piotrklis/experiment/MainActivity.java
@@ -15,16 +15,38 @@ import butterknife.OnClick;
 public class MainActivity extends AppCompatActivity {
 
     private static final int REQUEST_CODE = 1;
+    private static final String SAVED_INSTANCE_CONSTANT = "SavedInstanceConstant";
     @BindView(R.id.buttonMain)
     Button button;
-    @BindView(R.id.textView)
-    TextView textView;
+    @BindView(R.id.changableTextView)
+    TextView changeableTextView;
+    @BindView(R.id.saveInstanceTextView)
+    TextView saveInstanceTextView;
+    String saveInstanceContent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
+        if (savedInstanceState != null) {
+            saveInstanceContent = savedInstanceState.getString(SAVED_INSTANCE_CONSTANT);
+            setChangableTextView(saveInstanceContent);
+        } else {
+            saveInstanceContent = "content";
+            setChangableTextView(saveInstanceContent);
+        }
+    }
+
+    private void setChangableTextView(String content) {
+        changeableTextView.setText(content);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putString(SAVED_INSTANCE_CONSTANT, saveInstanceContent);
     }
 
     @Override
@@ -32,7 +54,8 @@ public class MainActivity extends AppCompatActivity {
         if (requestCode == REQUEST_CODE) {
             if (resultCode == Activity.RESULT_OK) {
                 String result = data.getStringExtra("content");
-                textView.setText(result);
+                changeableTextView.setText(result);
+                saveInstanceContent = result;
             }
             if (resultCode == Activity.RESULT_CANCELED) {
             }
